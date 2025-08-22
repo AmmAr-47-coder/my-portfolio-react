75a6444b85f4cdc708162659ea56a851b337148e
AmmAr Shabaan <shabaanammar597@gmail.com>
2025-08-22 15:04:23 +0300

    Update loading.jsx

--- src/loading.jsx
+++ src/loading.jsx
@@ -3,7 +3,9 @@ function loading() {
   return (
     <div>
       <div className="lo">
+        <h1 className="tu">{('</>')}</h1>
         <h1 className="loo">Welcome to my site</h1>
+        <div className="par" ></div>
       </div>
     </div>
   );
